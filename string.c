3ea62b84a2209c36d17ec099fab5953c0cb2f2c8
poojabujju <31477611+poojabujju@users.noreply.github.com>
2017-09-03 11:23:14 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,21 @@
+int main() {
+   char s1[30], s2[30];
+   int i;
+ 
+   printf("\nEnter two strings :");
+   gets(s1);
+   gets(s2);
+ 
+   i = 0;
+   while (s1[i] == s2[i] && s1[i] != '\0')
+      i++;
+   if (s1[i] > s2[i])
+      printf("s1 > s2");
+   else if (s1[i] < s2[i])
+      printf("s1 < s2");
+   else
+      printf("s1 = s2");
+ 
+   return (0);
+}
+
